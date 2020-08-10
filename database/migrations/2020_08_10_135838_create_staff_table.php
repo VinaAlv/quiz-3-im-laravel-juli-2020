9d2ae3f5f3607bbcd23c96b2a949a646533f9223
alvionita108 <60423164+alvionita108@users.noreply.github.com>
2020-08-10 21:09:48 +0700

    kuis 3

---
+++ database/migrations/2020_08_10_135838_create_staff_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama',30);
+
+            $table->unsignedBigInteger('proyek_id');
+            $table->foreign('proyek_id')->references('id')->on('proyek');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
